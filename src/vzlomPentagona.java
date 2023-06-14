91a367c2c83438e237cc781a2cc91d65a85127bd
Eugene Bubnov <eugenebubnov@vk.com>
2023-06-14 20:39:26 +0300

    Прикол для коммита, я взломал ничего.

---
+++ src/vzlomPentagona.java
@@ -0,0 +1,10 @@
+public class vzlomPentagona {
+    public static void main(String[] args){
+        int x;
+
+        for(x = 0; x < 101; x++){
+            System.out.println("Идёт процесс взлома Пентагона: " + x +"%");
+        }
+        System.out.println("Пентагон взломан!!!");
+    }
+}
