f3ebdaff138955c5a2089aa95163e0f9e2202b35
Eugene Bubnov <eugenebubnov@vk.com>
2023-06-17 16:36:15 +0300

    Switch book

---
+++ src/Displayer.java
@@ -0,0 +1,5 @@
+public class Displayer {
+    public static void main(String args[]){
+        System.out.println("Продолжаем изучать Java, но уже с литературой от Барри Берда!");
+    }
+}
--- src/Example_1.java
+++
@@ -1,9 +0,0 @@
-public class Example_1 {
-    // Однострочный комментарий.
-    /*
-    Многострочный комментарий.
-     */
-    public static void main(String[] args){
-        System.out.println("Java правит интернетом!");
-    }
-}
