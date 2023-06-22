507086ce8cb78e736989849644f10b3b3e2c64f3
Eugene Bubnov <eugenebubnov@vk.com>
2023-06-22 08:21:53 +0300

    Increment (++i, i , i++)

--- src/Inc.java
+++ src/Inc.java
@@ -2,8 +2,11 @@ public class Inc {
     public static void main(String args[]){
         int i = 10;
 
-        System.out.println(++i);
-        System.out.println(--i);
-        System.out.println(i);
+        System.out.println("Значение переменной по умолчанию: " + i);
+        System.out.println("Префикс инкремента равен: " + ++i); // Префиксная форма инкремента
+        System.out.println("Постфикс инкремента равен: " + i++ +"\nПосле i++, в следующем выражении переменная получится i = i + 1");
+        System.out.println("Конечное значение переменной после префикса и постфикса равно: " + i);
+        System.out.println(i++); //На выводе значение = 12, при следующем выводе переменная = i + 1.
+        System.out.println(i); // i = 13
     }
 }
