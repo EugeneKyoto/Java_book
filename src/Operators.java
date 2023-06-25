789fcea562f02b32b8d1b6e0bfd7c2e394f45248
Eugene Bubnov <eugenebubnov@vk.com>
2023-06-25 15:39:04 +0300

    Road to AQA. Operators again.

---
+++ src/Operators.java
@@ -0,0 +1,10 @@
+public class Operators {
+    public static void main(String args[]){
+        int i = 10;
+
+        i *= 2; // 20
+        i += 7; // 27
+        i /= 7; // 3
+        System.out.println(i);
+    }
+}
